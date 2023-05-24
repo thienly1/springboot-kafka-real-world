91e93388c907321328c63f1b3e4dd6ece3609d1b
THI LY TA <92649607+thienly1@users.noreply.github.com>
2023-05-24 15:02:58 +0200

    Initial commit

---
+++ src/main/java/net/javamicro/springboot/SpringbootKafkaRealWorldApplication.java
@@ -0,0 +1,13 @@
+package net.javamicro.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaRealWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaRealWorldApplication.class, args);
+	}
+
+}
